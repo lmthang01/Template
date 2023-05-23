14ba2888690fef21418a4a2f2bf81ccbca190c0b
HTNghia0503 <hatrungnghia0503@gmail.com>
2023-05-24 01:38:13 +0700

    Add Module Business - Nghia

---
+++ OrderManagement/View/Business/contract.php
@@ -0,0 +1,268 @@
+<!-- Preloader -->
+<div id="preloader">
+    <div class="loader"></div>
+</div>
+    
+<div class="page-container">
+        <!-- Main Content Area Start -->
+        <div class="main-content">
+            <!-- Header Area Start -->
+            <div class="header-area">
+                <div class="row align-items-center">
+                    <!-- Logo - Navigation - Notify - Setting - Profile Info -->
+                    <div class="">
+                        <?php
+                            include "navigation.php";
+                        ?>
+                    </div>    
+                </div>
+            </div>
+            <!-- Header Area End -->
+
+            <!-- Page Title Area Start -->
+            <div class="page-title-area">
+                <div class="row align-items-center">
+                    <div class="col-sm-6">
+                        <!-- Icon Sidebar -->
+                        <a href="">
+                            <i class="fa-solid fa-bars"></i>
+                        </a>
+                        <!-- Page Title -->
+                        <div class="breadcrumbs-area clearfix">
+                            <h4 class="page-title pull-left">Hợp đồng bán ra (5)</h4>
+                        </div>
+                    </div>
+
+                    <!-- Options Button -->
+                    <div class="crud-button">
+                        <form action="">
+                            <input type="button" value="Thêm mới">
+                            <input type="button" value="Tra cứu" >
+                            <input type="button" value="...">
+                        </form>
+			        </div>
+                </div>
+            </div>
+            <!-- Page Title Area End -->
+
+            <div class="main-content-inner">
+                <div class="row">
+                    <!-- Data Table Start -->
+                    <div class="col-12 mt-5">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="data-tables">
+                                    <table id="dataTable" class="text-center">
+                                        <thead class="bg-light text-capitalize">
+                                            <tr>
+                                                <th>Tên giao dịch</th>
+                                                <th>Mô tả</th>
+                                                <th>Đánh giá</th>
+                                                <th>Người phụ trách</th>
+                                                <th>Khách hàng</th>
+                                                <th>Loại giao dịch</th>
+                                                <th>Người liên hệ</th>
+                                                <th>Thời gian thực hiện</th>
+                                                <th>Trạng thái</th>
+                                                <th>Kết quả</th>
+                                                <th>Ưu tiên</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Airi Satou</td>
+                                                <td>Accountant</td>
+                                                <td>Tokyo</td>
+                                                <td>33</td>
+                                                <td>2008/11/28</td>
+                                                <td>$162,700</td>
+                                                <td>Accountant</td>
+                                                <td>Tokyo</td>
+                                                <td>33</td>
+                                                <td>2008/11/28</td>
+                                                <td>$162,700</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Angelica Ramos</td>
+                                                <td>Chief Executive Officer (CEO)</td>
+                                                <td>London</td>
+                                                <td>47</td>
+                                                <td>2009/10/09</td>
+                                                <td>$1,200,000</td>
+                                                <td>Accountant</td>
+                                                <td>Tokyo</td>
+                                                <td>33</td>
+                                                <td>2008/11/28</td>
+                                                <td>$162,700</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Ashton Cox</td>
+                                                <td>Junior Technical Author</td>
+                                                <td>San Francisco</td>
+                                                <td>66</td>
+                                                <td>2009/01/12</td>
+                                                <td>$86,000</td>
+                                                <td>Accountant</td>
+                                                <td>Tokyo</td>
+                                                <td>33</td>
+                                                <td>2008/11/28</td>
+                                                <td>$162,700</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Bradley Greer</td>
+                                                <td>Software Engineer</td>
+                                                <td>London</td>
+                                                <td>41</td>
+                                                <td>2012/10/13</td>
+                                                <td>$132,000</td>
+                                                <td>Accountant</td>
+                                                <td>Tokyo</td>
+                                                <td>33</td>
+                                                <td>2008/11/28</td>
+                                                <td>$162,700</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Brenden Wagner</td>
+                                                <td>Software Engineer</td>
+                                                <td>San Francisco</td>
+                                                <td>28</td>
+                                                <td>2011/06/07</td>
+                                                <td>$206,850</td>
+                                                <td>Accountant</td>
+                                                <td>Tokyo</td>
+                                                <td>33</td>
+                                                <td>2008/11/28</td>
+                                                <td>$162,700</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Caesar Vance</td>
+                                                <td>Pre-Sales Support</td>
+                                                <td>New York</td>
+                                                <td>29</td>
+                                                <td>2011/12/12</td>
+                                                <td>$106,450</td>
+                                                <td>Accountant</td>
+                                                <td>Tokyo</td>
+                                                <td>33</td>
+                                                <td>2008/11/28</td>
+                                                <td>$162,700</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Bruno Nash</td>
+                                                <td>Software Engineer</td>
+                                                <td>Edinburgh</td>
+                                                <td>21</td>
+                                                <td>2012/03/29</td>
+                                                <td>$433,060</td>
+                                                <td>Accountant</td>
+                                                <td>Tokyo</td>
+                                                <td>33</td>
+                                                <td>2008/11/28</td>
+                                                <td>$162,700</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Bradley Greer</td>
+                                                <td>Software Engineer</td>
+                                                <td>London</td>
+                                                <td>41</td>
+                                                <td>2012/10/13</td>
+                                                <td>$132,000</td>
+                                                <td>Accountant</td>
+                                                <td>Tokyo</td>
+                                                <td>33</td>
+                                                <td>2008/11/28</td>
+                                                <td>$162,700</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Brenden Wagner</td>
+                                                <td>Software Engineer</td>
+                                                <td>San Francisco</td>
+                                                <td>28</td>
+                                                <td>2011/06/07</td>
+                                                <td>$206,850</td>
+                                                <td>Accountant</td>
+                                                <td>Tokyo</td>
+                                                <td>33</td>
+                                                <td>2008/11/28</td>
+                                                <td>$162,700</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Caesar Vance</td>
+                                                <td>Pre-Sales Support</td>
+                                                <td>New York</td>
+                                                <td>29</td>
+                                                <td>2011/12/12</td>
+                                                <td>$106,450</td>
+                                                <td>Accountant</td>
+                                                <td>Tokyo</td>
+                                                <td>33</td>
+                                                <td>2008/11/28</td>
+                                                <td>$162,700</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Bruno Nash</td>
+                                                <td>Software Engineer</td>
+                                                <td>Edinburgh</td>
+                                                <td>21</td>
+                                                <td>2012/03/29</td>
+                                                <td>$433,060</td>
+                                                <td>Accountant</td>
+                                                <td>Tokyo</td>
+                                                <td>33</td>
+                                                <td>2008/11/28</td>
+                                                <td>$162,700</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <div class="card-footer-line">
+                                    <span>Nợ:</span>
+                                    <div class="order-result">
+                                        <span>13.980.570</span>
+                                    </div>
+                                </div>
+                                <div class="card-footer-line">
+                                    <span>Đã TT:</span>
+                                    <div class="order-result">
+                                        <span>0</span>
+                                    </div>
+                                </div>
+                                <div class="card-footer-line">
+                                    <span>Giá trị HĐ:</span>
+                                    <div class="order-result">
+                                        <span>13.980.570</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- main content area end -->
+    </div>
+    <!-- page container area end -->
+    <!-- jquery latest version -->
+    <script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
+    <!-- bootstrap 4 js -->
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/metisMenu.min.js"></script>
+    <script src="assets/js/jquery.slimscroll.min.js"></script>
+    <script src="assets/js/jquery.slicknav.min.js"></script>
+
+    <!-- Start datatable js -->
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+    <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap.min.js"></script>
+    <!-- others plugins -->
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/scripts.js"></script>
+</body>
+
+</html>
---
+++ OrderManagement/View/Business/navigation.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Order Management</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="CSS/base.css">
+    <link rel="stylesheet" href="CSS/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/fontawesome.min.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <a href="#" class="logo">
+                <img src="https://crmviet.vn/wp-content/uploads/2020/04/logo-crmviet.png" alt="Logo" height="30" width="auto">
+            </a>
+        </div>
+        <ul class="nav_main">
+            <li>
+                <a href="#">Khách hàng</a>
+                <ul class="subnav_main">
+                    <li><a href="">Khách hàng</a></li>
+                    <li><a href="">Liên hệ với khách hàng</a></li>
+                    <li><a href="">List khách hàng</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Kinh doanh</a>
+                <ul class="subnav_main">
+                    <li><a href="">Giao dịch với khách hàng</a></li>
+                    <li><a href="">Đơn hàng</a></li>
+                    <li><a href="">Hợp đồng bán ra</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Báo cáo</a>
+                <ul class="subnav_main">
+                    <li><a href="">Biểu đồ</a></li>
+                </ul>
+            </li>		
+        </ul>
+        <ul class="notification-area pull-right">
+            <!-- Thông báo -->
+            <li class="dropdown">
+                <i class="fa-solid fa-bell"></i>
+            </li>
+            <!-- Setting -->
+            <li class="settings-btn">
+                <i class="fa-solid fa-gear"></i>
+            </li>
+        </ul>
+        <!-- Profile Info -->
+        <div class="nav_user">   
+            <a href="#">demo<img id="avt" src=""> </a>
+            <ul class="subnav_user">
+                <li><a href="">Hướng dẫn sử dụng</a></li>
+                <li><a href="">Thông tin cá nhân</a></li>
+                <li><a href="">Cài đặt Email</a></li>
+                <li><a href="">Hòm thư</a></li>
+                <li><a href="">Trợ giúp</a></li>
+                <li><a href="">Thoát</a></li>
+            </ul>
+        </div>
+    </header>
+</body>
+</html>
